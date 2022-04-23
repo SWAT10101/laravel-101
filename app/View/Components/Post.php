ca2a0d93dc73b1676f0f5d2f8bc39b4ce8e2031c
unknown <swat1994911@gmail.com>
2022-04-23 05:27:11 +0300

    laravel mix

--- app/View/Components/Post.php
+++ app/View/Components/Post.php
@@ -6,6 +6,7 @@ use Illuminate\View\Component;
 
 class Post extends Component
 {
+
     public $post;
     public $comments;
     /**
@@ -26,44 +27,6 @@ class Post extends Component
      */
     public function render()
     {
-        return <<<'blade'
-
-
-  <main role="main" class="container">
-    <div class="row">
-      <div class="col-md-8 blog-main">
-  
-  
-          <div class="blog-post mb-3">
-              <h2 class="blog-post-title">
-                 {{($post->title)}}
-              </h2>
-             <p class="blog-post-meta">
-                 بقلم{{($post->author)}}
-                 {{ (Carbon\Carbon::parse($post->created_at)->diffForHumans())}}
-              </p>
-              <p>
-                 {{($post->body)}}
-              </p>
-  
-              <h1>التعليقات</h1>
-              @if (count($comments) == 0)
-                   <h3>لا يوجد تعليقات</h3>
-              @else
-  
-               @foreach ($comments as $comment)
-               <h4>
-                     {{($comment->name)}}
-               </h4>
-   
-               <p>
-                {{($comment->body)}}
-               </p>
-               @endforeach
-              @endif
-  
-            </div>
-          
-blade;
+        return view('components.post');
     }
 }
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -6,8 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="https://cdn.rtlcss.com/bootstrap/v4.5.3/css/bootstrap.min.css"
-        integrity="sha384-JvExCACAZcHNJEc7156QaHXTnQL3hQBixvj5RV5buE7vgnNEzzskDtx9NQ4p6BJe" crossorigin="anonymous">
+   
 
 </head>
 
